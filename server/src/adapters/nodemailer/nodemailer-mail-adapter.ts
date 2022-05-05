c55bed4c4bec1ddccc82a440f719fea49ddb5909
Lailson Sobral <lailsonlm@hotmail.com>
2022-05-05 12:26:08 -0300

    create backend

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,23 @@
+import nodemailer from 'nodemailer'
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "86ad0ba9f28e87",
+    pass: "7c05463564ca86"
+  }
+});
+
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Lailson Sobral <lailsonlm@hotmail.com>',
+      subject,
+      html: body,
+    })
+  }
+}
---
+++ server/src/server.ts
@@ -0,0 +1,15 @@
+import cors from 'cors';
+import express from 'express'
+import { routes } from './router';
+
+const app = express();
+
+app.use(cors({
+  // origin: 'https://localhost:3000'
+}));
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('HTTP server running!')
+})
